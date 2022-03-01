9125df52b488ba76919eea0224c47b31402d886a
Yves Tscherry <info@feerglas.com>
2022-03-01 15:54:38 +0100

    try to hydrate one sample route

---
+++ pages/_document.js
@@ -0,0 +1,59 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { parse } from 'node-html-parser';
+import { renderToString } from 'react-dom/server'
+
+const lyneComps = require("lyne-test/hydrate");
+
+class MyDocument extends Document {
+
+  static async getInitialProps(ctx) {
+
+    const initialProps = await Document.getInitialProps(ctx);
+    let styleContent = '';
+
+    if (ctx.pathname === '/kitchensink1') {
+
+      // hydrate the received html via stencil's renderToString method
+      const renderedHtml = await lyneComps.renderToString(initialProps.html);
+
+      // stencil is creating an fully fledged html document out of the component
+      // html after hydration. we need to parse it and extract the contents
+      // of the body.
+      const parsedHtml = parse(renderedHtml.html);
+      const renderedContent = parsedHtml.querySelector('#__next').toString();
+      initialProps.html = renderedContent;
+
+      // we need to extract the css from the head of the hydrated result.
+      const headChildNodes = parsedHtml.getElementsByTagName('head')[0].childNodes;
+      headChildNodes.forEach((childNode) => {
+        if (childNode.rawTagName === 'style') {
+          styleContent += `${childNode.childNodes[0]._rawText}`;
+        }
+      });
+    }
+
+    return {
+      ...initialProps,
+      styles: (
+        <style>
+          {initialProps.styles}
+          {styleContent}
+        </style>
+      )
+    };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
