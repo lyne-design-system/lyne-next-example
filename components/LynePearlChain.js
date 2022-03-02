4c89bdcdf33f04b82e80cfeb2b4d93ba17adb58a
Yves Tscherry <info@feerglas.com>
2022-03-02 13:38:14 +0100

    use more complex legs config

--- components/LynePearlChain.js
+++ components/LynePearlChain.js
@@ -1,7 +1,7 @@
 export default function LyneButton() {
   return (
     <lyne-pearl-chain
-      legs='{"legs":[{"cancellation":false,"duration":100}]}'
+      legs='{"legs":[{"cancellation":false,"duration":10},{"cancellation":false,"duration":5},{"cancellation":false,"duration":5},{"cancellation":false,"duration":10},{"cancellation":false,"duration":10},{"cancellation":false,"duration":5},{"cancellation":false,"duration":5},{"cancellation":false,"duration":10},{"cancellation":false,"duration":15},{"cancellation":false,"duration":25}]}'
       status="future"
       suppressHydrationWarning={true}
     ></lyne-pearl-chain>
